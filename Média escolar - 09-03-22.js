52fd933c1c38585f61da0fc12bb3c6d922a5d5d0
Louz20 <100964659+Louz20@users.noreply.github.com>
2022-06-03 17:05:12 -0300

    Add files via upload
    Este foi o meu primeiro projeto a ser criado, bem simples, porém, me serviu de muito aprendizado!

---
+++ Média escolar - 09-03-22.js
@@ -0,0 +1,17 @@
+nota1= 4.0
+nota2= 8.0
+
+media = (nota1 + nota2) /2
+
+if (media >= 7)
+
+{
+console.log('aprovado', media)
+console.log('parabéns!!! :)') 
+}
+
+else {
+console.log('reprovado', media)
+
+console.log('estude mais da próxima! :(')
+}
